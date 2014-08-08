e2cfa631623f837b06745e5e0e40a64c6ec313fc
Steve Seeley <sseeley@merchantwarehouse.com>
2014-08-08 14:20:01 -0400

    Squashed 'hello/' content from commit 7a7b9fd
    git-subtree-dir: hello
    git-subtree-split: 7a7b9fd45a0b62d85e89dec690c29dedff2fc43f

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,16 @@
+namespace ConsoleApplication5
+{
+
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+
+    }
+
+}
